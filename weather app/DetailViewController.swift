9f675f72a6cc8ac7bf5010e01931756e75a35aad
oktydeniz <oktydeniz07@gmail.com>
2021-10-19 22:18:25 +0300

    Initial Commit

---
+++ weather app/DetailViewController.swift
@@ -0,0 +1,60 @@
+//
+//  DetailViewController.swift
+//  weather app
+//
+//  Created by oktay on 19.10.2021.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    var city  = String()
+    @IBOutlet weak var cityWeather: UILabel!
+    @IBOutlet weak var cityName: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        cityName.text = city
+        getTodayWeatherResult(c: city)
+        // Do any additional setup after loading the view.
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.navigationBar.isHidden = false
+    }
+    
+    
+    func getTodayWeatherResult(c:String){
+        //93406ea9dee5c1041592dccc02cf0d8f
+        if let url =  URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(c)&appid=93406ea9dee5c1041592dccc02cf0d8f"){
+            let request = URLRequest(url:url)
+            let task =  URLSession.shared.dataTask(with: request){(data,response,error) in
+                
+                if error == nil{
+                    if let currentdata = data {
+                        do{
+                            let json = try JSONSerialization.jsonObject(with: currentdata, options: JSONSerialization.ReadingOptions.mutableContainers) as! [String:String]
+                            
+                            if let main = json["main"] as? NSDictionary{
+                               if let temp = main["temp"] as? Double{
+                                   let state = Int(temp - 273.15)
+                                   DispatchQueue.main.sync {
+                                       self.cityWeather.text = String(state)
+                                   }
+                                }
+                                
+                            }
+                        }catch{
+                            print("Some thing not right")
+                        }
+                    }
+                }
+            }
+            task.resume()
+            
+        }
+        
+        
+    }
+    
+
+}
---
+++ weather app/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  weather app
+//
+//  Created by oktay on 19.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var cityArea: UITextField!
+    @IBOutlet weak var showWeather: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        showWeather.layer.cornerRadius = 6
+        // Do any additional setup after loading the view.
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.navigationBar.isHidden = true
+    }
+    
+
+    @IBAction func showWeatherAction(_ sender: Any) {
+        let city = cityArea.text ?? ""
+        
+        if city.isEmpty {
+            let alert = UIAlertController(title: "Uyari", message: "Sehir adini giriniz", preferredStyle: .alert)
+            let cancelBtn = UIAlertAction(title: "Cancel", style: .cancel,handler: nil)
+            alert.addAction(cancelBtn)
+            self.present(alert,animated: true,completion: nil)
+        }
+        else{
+            let vc = self.storyboard?.instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
+            
+            vc.city = city
+            self.show(vc,sender:nil)
+        }
+    }
+    
+}
+
